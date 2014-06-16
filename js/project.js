43195d39adca047696305d2d07ce3fb0e693af8d
ncmochacity <nickichenbattle@gmail.commkdir ~/myprojectgit config --global user.email nickichenbattle@gmail.com>
2014-06-15 23:21:53 -0500

    finished project

--- js/project.js
+++ js/project.js
@@ -14,7 +14,43 @@ $(document).ready(function(){
 
 			$('.icon span').addClass('animate');
 
-	}
+		}
+		if(y >=1000){
+			
+			$("#s1").addClass("animate-down");
+			setTimeout(function(){
+				$("#s2").addClass("animate-down");
+			},100);
+			setTimeout(function(){
+				$("#s3").addClass("animate-down");
+			},200);
+			setTimeout(function(){
+				$("#s4").addClass("animate-down");
+			},300);
+			setTimeout(function(){
+				$("#s5").addClass("animate-down");
+			},400);
+			setTimeout(function(){
+				$("#s6").addClass("animate-down");
+			},500);
+			setTimeout(function(){
+				$("#s7").addClass("animate-down");
+			},600);
+			setTimeout(function(){
+				$("#s8").addClass("animate-down");
+			},700);
+			setTimeout(function(){
+				$("#s9").addClass("animate-down");
+			},800);
+			setTimeout(function(){
+				$("#s10").addClass("animate-down");
+			},900);
+
+		}
+		if(y>=1200){
+			$("#servers").addClass("animate");
+			$("#offer").addClass("animate");
+		}
 	});
 	
 		
